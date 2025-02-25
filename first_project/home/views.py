3ee3d4381eaf2fa6769ffb1ad0e77980293ad246
Ayush04H <ayush050419@gmail.com>
2025-02-25 11:29:22 +0530

    Basic View and Urls Tutorial

--- first_project/home/views.py
+++ first_project/home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+'''def home(request):
+    return HttpResponse("<h1>This is My Django Server ! Welcome to the First Program</>")'''
+
+def home(request):
+    return render(request,"index.html")
+
+
+def test(request):
+    return HttpResponse("This is Another Function of the Django")
